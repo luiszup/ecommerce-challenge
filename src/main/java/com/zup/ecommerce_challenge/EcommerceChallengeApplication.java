13c83684801c4a221aa24b798ed961c68b7cc5ff
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-01-28 16:11:27 -0300

    first commit

---
+++ src/main/java/com/zup/ecommerce_challenge/EcommerceChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.zup.ecommerce_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zup/ecommerce_challenge/EcommerceChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.ecommerce_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
