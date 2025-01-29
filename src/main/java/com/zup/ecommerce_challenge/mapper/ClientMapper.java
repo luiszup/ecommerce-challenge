c0c72224681e31604ad1fb2bb1fdc3df9abc7b3c
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-01-29 15:36:41 -0300

    feat: add createClient and getAllClients methods on ClientService and add convertModelToDTO method on ClientMapper

--- src/main/java/com/zup/ecommerce_challenge/mapper/ClientMapper.java
+++ src/main/java/com/zup/ecommerce_challenge/mapper/ClientMapper.java
@@ -4,6 +4,10 @@ import com.zup.ecommerce_challenge.dto.ClientDTO;
 import com.zup.ecommerce_challenge.model.Client;
 
 public class ClientMapper {
+    public static ClientDTO convertModelToDTO(Client client) {
+        return new ClientDTO(client.getName(), client.getEmail(), client.getCpf());
+    }
+
     public static Client convertDTOtoModel(ClientDTO clientDTO) {
         if (clientDTO == null) {
             throw new IllegalArgumentException("O cliente não pode ser nulo");
--- src/main/java/com/zup/ecommerce_challenge/service/ClientService.java
+++ src/main/java/com/zup/ecommerce_challenge/service/ClientService.java
@@ -1,10 +1,14 @@
 package com.zup.ecommerce_challenge.service;
 
 import com.zup.ecommerce_challenge.dto.ClientDTO;
+import com.zup.ecommerce_challenge.mapper.ClientMapper;
 import com.zup.ecommerce_challenge.model.Client;
 import com.zup.ecommerce_challenge.repository.ClientRepository;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ClientService {
@@ -14,5 +18,17 @@ public class ClientService {
     public ClientService(ClientRepository clientRepository, ProductService productService) {
         this.clientRepository = clientRepository;
         this.productService = productService;
+    }
+
+    public Client createClient(@Valid ClientDTO clientDTO) {
+        Client client = ClientMapper.convertDTOtoModel(clientDTO);
+        return clientRepository.save(client);
+    }
+
+    public List<ClientDTO> getAllClients() {
+        List<Client> clients = clientRepository.findAll();
+        return clients.stream()
+                .map(ClientMapper::convertModelToDTO)
+                .collect(Collectors.toList());
     }
 }
