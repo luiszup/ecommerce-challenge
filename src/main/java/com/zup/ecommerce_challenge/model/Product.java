f22a92921a58803a443db0eb8c51b0c84fe46954
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-01-29 10:39:09 -0300

    feat: add Product model

---
+++ src/main/java/com/zup/ecommerce_challenge/model/Product.java
@@ -0,0 +1,43 @@
+package com.zup.ecommerce_challenge.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+
+@Entity
+public class Product {
+
+    @Column(unique = true)
+    private String name;
+
+    @DecimalMin(value = "0.01", message = "O preço do produto deve ser maior que 0.")
+    private Double price;
+
+    @Min(value = 0, message = "A quantidade em estoque deve ser maior ou igual a 0.")
+    private int quantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
