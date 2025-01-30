5e575b6f4a60e1c41e094eef8e5b7fa991e13787
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-01-30 09:32:11 -0300

    feat: add constructor in ProductServices and findById method in ProductRepository

--- src/main/java/com/zup/ecommerce_challenge/service/ProductService.java
+++ src/main/java/com/zup/ecommerce_challenge/service/ProductService.java
@@ -1,4 +1,13 @@
 package com.zup.ecommerce_challenge.service;
 
+import com.zup.ecommerce_challenge.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 }
