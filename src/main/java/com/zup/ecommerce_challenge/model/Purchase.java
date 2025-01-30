f21775cbebdfcc3b2ada9a673ce279a12c9a7bdd
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-01-30 14:12:38 -0300

    feat: add purchase model

---
+++ src/main/java/com/zup/ecommerce_challenge/model/Purchase.java
@@ -0,0 +1,67 @@
+package com.zup.ecommerce_challenge.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class Purchase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    @ManyToMany
+    @JoinTable(
+            name = "purchase_products",
+            joinColumns = @JoinColumn(name = "purchase_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products;
+
+    @Column(nullable = false)
+    private LocalDateTime purchaseDate;
+
+    public Purchase(Client client, Long id, List<Product> products, LocalDateTime purchaseDate) {
+        this.client = client;
+        this.id = id;
+        this.products = products;
+        this.purchaseDate = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public LocalDateTime getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(LocalDateTime purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+}
