e0e3cca14cd0dbd9b19b83ef17cf0727c5efe12f
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-01-31 11:32:57 -0300

    feat: add getPurchaseById method

--- src/main/java/com/zup/ecommerce_challenge/controller/PurchaseController.java
+++ src/main/java/com/zup/ecommerce_challenge/controller/PurchaseController.java
@@ -5,10 +5,7 @@ import com.zup.ecommerce_challenge.dto.PurchaseResponseDTO;
 import com.zup.ecommerce_challenge.service.PurchaseService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/compras")
@@ -27,5 +24,10 @@ public class PurchaseController {
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().build();
         }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PurchaseResponseDTO> getPurchaseById(@PathVariable Long id) {
+        return ResponseEntity.ok(purchaseService.getPurchaseById(id));
     }
 }
