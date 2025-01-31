4b585f0f4c03ebe4322005877bb126e1a132ea60
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-01-31 11:05:32 -0300

    feat: add getProductById method

--- src/main/java/com/zup/ecommerce_challenge/controller/ProductController.java
+++ src/main/java/com/zup/ecommerce_challenge/controller/ProductController.java
@@ -4,10 +4,7 @@ import com.zup.ecommerce_challenge.dto.ProductDTO;
 import com.zup.ecommerce_challenge.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/produtos")
@@ -26,5 +23,10 @@ public class ProductController {
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().build();
         }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductById(id));
     }
 }
